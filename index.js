e7151b091375b416464c79672dfcec1d2ee6fdd2
rguim0 <donelliguilherme103@gmail.com>
2025-03-31 22:05:45 -0300

    v0.1

---
+++ index.js
@@ -0,0 +1,33 @@
+function conversor1() {
+
+    valoremreais = prompt ("Digite aqui a quantia que deseja converter!")
+    
+    simoleon = 6,67
+    alert("Você tem..." + " R$" + valoremreais * simoleon + " simoleons!") 
+      }
+    
+      function conversor2() {
+    
+    valoremdolar = prompt ("Digite aqui a quantia que deseja converter!")
+    
+    simoleon = 5,85
+    alert("Você tem..." + " USD" + valoremdolar * simoleon + " simoleons!") 
+      }
+    
+      function conversor3() {
+    
+    valoremeuro = prompt ("Digite aqui a quantia que deseja converter!")
+    
+    simoleon = 10,80
+    alert("Você tem..." + " €" + valoremeuro * simoleon + " simoleons!") 
+      }
+    
+      function conversor4() {
+    
+    valoremienes = prompt ("Digite aqui a quantia que deseja converter!")
+    
+    simoleon = 175,53
+    alert("Você tem..." + " ¥" + valoremienes * simoleon + " simoleons!") 
+      }
+    
+    // {chaves} = tudo que ta entre chaves é chamado de bloco.
